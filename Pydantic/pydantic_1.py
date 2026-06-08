78b8c98c5b0f00a04a3b623df43461ebed635e06
Ashutosh Singh <ashutoshsinghu2002@gmail.com>
2026-06-08 20:09:22 +0530

    Learning Pydantic

---
+++ Pydantic/pydantic_1.py
@@ -0,0 +1,16 @@
+""" A simple  """
+from pydantic import BaseModel
+
+class User(BaseModel):
+    id: int
+    name: str
+    is_active: bool
+
+input_data = {
+    'id': 123,  
+    'name': 'John Doe',
+    'is_active': True
+}
+
+user = User(**input_data)
+print(user)
